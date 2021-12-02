1497c9bba441c5c55fdbf05d5d8c91e29d790568
Dakota Madden-Fong <maddenfong@gmail.com>
2021-12-01 16:27:13 -0800

    added tests for Vector

--- src/engine/vector.test.ts
+++ src/engine/vector.test.ts
@@ -10,3 +10,23 @@ test('Gets and sets magnitude', () => {
     expect(t.x).toBeCloseTo(100 / Math.sqrt(2));
 
 });
+
+test('Gets and sets direction', () => {
+
+    const t = new Vector(1, 1);
+    expect(t.direction).toBeCloseTo(Math.PI / 4);
+    t.direction = Math.PI / 2;
+    expect(t.x).toBeCloseTo(0);
+    expect(t.y).toBeCloseTo(Math.sqrt(2));
+
+});
+
+test('Normalize a vector', () => {
+
+    const t = new Vector(100, 100);
+    const norm = t.normalized();
+    expect(norm.magnitude).toBeCloseTo(1);
+    expect(norm.direction).toBeCloseTo(t.direction);
+
+});
+
