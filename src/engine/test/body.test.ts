d9759c27404a958faa344c98f584b78e81871190
Dakota Madden-Fong <maddenfong@gmail.com>
2021-12-04 12:21:35 -0800

    set up tests for body

---
+++ src/engine/test/body.test.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-magic-numbers */
+
+import {describe, expect, test} from '@jest/globals';
+
+import {Rect, Circle} from '../body';
+
+// test('Getting and setting Rect properties', () => {
+
+// });
+
+// test('Getting and setting Circle properties', () => {
+
+// });
+
+// test('Rect to rect collision', () => {
+
+// });
+
+// test('Circle to circle collision', () => {
+
+// });
+
+// test('Rect to circle collision', () => {
+
+// });
